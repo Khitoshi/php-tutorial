f3521e312adba4a8c54944128a8975e9218c22f4
Khitoshi <hitoshi.kubota@link-u.co.jp>
2026-02-05 11:05:56 +0900

    fix:  404エラーハンドリングの改善

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ function setPaths($url, $path) {
 
 function getRoot($url) {
     global $mapped_paths;
-    return isset($mapped_paths[$url]) ? $mapped_paths[$url] : "echo '404 Not Found';";
+    return isset($mapped_paths[$url]) ? $mapped_paths[$url] : null;
 }
 
 $request_uri = $_SERVER['REQUEST_URI'];
@@ -17,15 +17,14 @@ $path = parse_url($request_uri, PHP_URL_PATH);
 setPaths('/login', 'login.php');
 setPaths('/table', 'table.php');
 setPaths('/logout', 'logout.php');
-setPaths('/newuser', 'newuser.php');
 setPaths('/comment', 'comment.php');
 
 $file = getRoot($path);
 
-if ($file === null) {
-    http_response_code(404);
-    echo "404 Not Found";
-    exit;
-}
+if ($file === null || !file_exists(__DIR__ . '/' . $file)) {  
+     http_response_code(404);  
+     echo "404 Not Found";  
+     exit;  
+ }  
 
 require __DIR__ . '/' . $file;
